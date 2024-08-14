1a88c9118e45120711826795611744c06b4d0e57
PranavKulkarni92 <115733722+PranavKulkarni92@users.noreply.github.com>
2024-08-14 11:56:19 +0530

    Add files via upload

---
+++ 4_unaryoperator.js
@@ -0,0 +1,15 @@
+console.log(+true);
+console.log(+"");
+console.log(!"JavaScript");
+
+/*
+concept:-unary operator
+
+Refer JavaScript.INfO
+
+unary operator + applied to a single value , doesn't do anything to numbers.
+But if operand is not a number , the unary plus converts it into a number.
+
+! negation tries to convert to boolean , truthy to false [which is he case here]
+and falsy to true
+*/
