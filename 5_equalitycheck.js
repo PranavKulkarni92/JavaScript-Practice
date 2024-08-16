dd95d8d25c85f9b7c4613c831950d7b68a76ed37
PranavKulkarni92 <115733722+PranavKulkarni92@users.noreply.github.com>
2024-08-16 20:42:43 +0530

    Add files via upload

---
+++ 5_equalitycheck.js
@@ -0,0 +1,18 @@
+let a = 10;
+let b = "10"
+let c = new Number(10);
+let d = 10;
+
+console.log(a == b);
+console.log(a == c);
+console.log(a === b);
+console.log(a === c);
+console.log(a == d);
+console.log(c === d);
+
+/*
+Concept:-equality check == and ===
+
+
+*/
+
