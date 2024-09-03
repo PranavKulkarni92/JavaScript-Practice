7c987021735064c39dd1b8b9a213e7fdd36e1467
PranavKulkarni92 <115733722+PranavKulkarni92@users.noreply.github.com>
2024-09-03 23:00:29 +0530

    Add files via upload

---
+++ 13_getTypeArray.js
@@ -0,0 +1,16 @@
+function getType(...args){
+    console.log(typeof args);
+}
+
+getType("108");
+
+/*
+concept:- 
+spread operator:- collecting all arguments in a special way using Spread operator.
+    Spread operator is used
+
+typeof operator:-
+
+To check whether anything is array or not ....using inbuilt method Array.isArray(args)
+
+*/
