5d949c2b8b59b9f735cac7f216a10c4d54b2dbfd
PranavKulkarni92 <115733722+PranavKulkarni92@users.noreply.github.com>
2024-09-05 20:01:38 +0530

    Add files via upload

---
+++ 14_useStrict.js
@@ -0,0 +1,12 @@
+function getAge(){
+    "use strict"
+    salary = 999;
+    console.log(salary);
+}
+
+getAge();
+
+/*
+Concept:- use strict enforces us to declare variables
+
+*/
---
+++ 15_varDecl.js
@@ -0,0 +1,21 @@
+
+var num = 11;
+
+var num = 9;
+
+//let num = 11;
+
+//let num = 9;
+
+
+
+console.log(num);
+
+/*
+Concept:- 
+
+with VAR we can declare variables with same name, & 
+variable declared in last will be used
+
+with LET and CONST , we cannot declare variables with same name
+*/
